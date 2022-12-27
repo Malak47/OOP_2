40598b0b020ae6f8b79cbcde40021864d22a32cd
Lara Abu Hamad <laraabuhamad@gmail.com>
2022-12-27 18:12:00 +0200

    Creating functions.

--- Ex2_1.java
+++ Ex2_1.java
@@ -1,3 +1,19 @@
 public class Ex2_1 {
 
+    public static String[] createTextFiles(int n, int seed, int bound){
+        return new String[]{""};
+    }
+
+    public static int getNumOfLines(String[] fileNames){
+        return 0;
+    }
+
+    public int getNumOfLinesThreads(String[] fileNames){
+        return 0;
+    }
+
+    public int getNumOfLinesThreadPool(String[] fileNames){
+        return 0;
+    }
+
 }
