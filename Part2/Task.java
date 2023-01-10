5b15b9d70bc175a46ee38f758bc26a9420804a09
malak <malak.qeedan@hotmail.com>
2023-01-10 22:12:03 +0200

    updating Task, v.3.2 Task now extends FutureTask, implements Callable and Comparable. Some tweaks to make it work…

--- Part2/Task.java
+++ Part2/Task.java
@@ -1,47 +1,48 @@
 package Part2;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
 
-public class Task<T> implements Comparable<Task> {
-    private final Callable<T> task;
-    private final TaskType type;
-    private int priority;
+public class Task<T> extends FutureTask<T> implements Callable<T>, Comparable<Task<T>> {
 
-    private Task(Callable<T> task, TaskType type) {
-        this.task = task;
-        this.type = type;
-        this.priority = type.getPriorityValue();
+    private final Callable<T> callable;
+    private final TaskType taskType;
+
+    public Task(Callable<T> callable, TaskType taskType) {
+        super(callable);
+        this.callable = callable;
+        this.taskType = taskType;
     }
 
-    public static <T> Task<T> createTask(Callable<T> task, TaskType type) {
-        return new Task<>(task, type);
+    public static <T> Task<T> createTask(Callable<T> callable, TaskType taskType) {
+        return new Task<>(callable, taskType);
     }
 
-    public static <T> Task<T> createTask(Callable<T> task) {
-        // Set the default TaskType to COMPUTATIONAL
-        return new Task<>(task, TaskType.COMPUTATIONAL);
+    public static <T> Task<T> createTask(Callable<T> callable) {
+        return new Task<>(callable, TaskType.OTHER);
     }
 
-    public Callable<T> getTask() {
-        return task;
+    public TaskType getTaskType() {
+        return taskType;
     }
 
-    public TaskType getType() {
-        return type;
-    }
-
-    public void setPriority(int priority) {
-        this.priority = priority;
-    }
-
-    public int getPriorityValue() {
-        return priority;
+    public int getPriority() {
+        return this.taskType.getPriorityValue();
     }
 
     @Override
-    public int compareTo(Task other) {
-        return Integer.compare(this.priority, other.priority);
+    public T call() throws Exception {
+        return callable.call();
+    }
+
+    @Override
+    public int compareTo(Task<T> other) {
+        return Integer.compare(taskType.getPriorityValue(), other.taskType.getPriorityValue());
+    }
+
+    @Override
+    public String toString() {
+        String s = super.toString();
+        return s.substring(s.indexOf('['), s.indexOf(']')) + ", task type = " + this.taskType + "]";
     }
 }
-
-
